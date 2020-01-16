cbcfdffc3554c8c95f8b1b1df3b380ffc2e2a869
kk <k.klapczynski@gmail.com>
2020-01-16 10:39:17 +0100

    198: Set value of control.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // defaultName = '';
   defaultQuestion = 'teacher';
   questionAnswer = '';
   genders = ['male', 'female'];
@@ -18,5 +19,20 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.defaultName = suggestedName;  // simple solution to set just one control 
+    // this.signupForm.setValue({            // demo solution, but all controls of form must be set and are updated/overwritten
+    //   'userData': {
+    //     'username': suggestedName,
+    //     'email': ''
+    //   },
+    //     'secret': '',
+    //     'answer': '',
+    //     'gender': 'male'
+    //   });
+    this.signupForm.form.patchValue({   // allowes to set single control value
+      'userData': {
+        'username': suggestedName
+      }
+    })
   }
 }
