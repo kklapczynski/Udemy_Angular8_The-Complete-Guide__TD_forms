d2b4fec2574036a6408fad39bcda9025f0d8a287
kk <k.klapczynski@gmail.com>
2020-01-16 11:23:05 +0100

    199: Submit a form and reset it. Correct invalid control  style.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,22 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   // defaultName = '';
+  submitted = false;
   defaultQuestion = 'teacher';
   questionAnswer = '';
   genders = ['male', 'female'];
   @ViewChild('f', {static: false}) signupForm: NgForm;
+  user={'name':'', 'email': '', 'secret': '', 'answer': '', 'gender': ''};
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.name = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.answer;
+    this.user.gender = this.signupForm.value.gender;
+    
+    this.signupForm.reset();
   }
 
   suggestUserName() {
